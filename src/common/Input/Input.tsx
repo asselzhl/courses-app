9e9f6561180cfbf5b9f76cd857cb4c4d97d7ec79
asselzhl <asselzhly@gmail.com>
2024-04-09 18:13:28 +0500

    implement SearchBar component

--- src/common/Input/Input.tsx
+++ src/common/Input/Input.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React from 'react';
 
 export const Input = ({
-  type,
-  labelText,
-  placeholderText,
-  inputID,
-  onChange,
+	type,
+	labelText,
+	placeholderText,
+	inputID,
+	onChange,
 }) => {
-  return (
-    <>
-      {labelText && <label htmlFor={inputID}>{labelText}</label>}
-      <input
-        type={type}
-        id={inputID}
-        placeholder={placeholderText}
-        className="py-3 px-4 leading-6 rounded border border-[#CFCFCF] focus:outline-[#007298] w-full"
-      />
-    </>
-  );
+	return (
+		<>
+			{labelText && <label htmlFor={inputID}>{labelText}</label>}
+			<input
+				type={type}
+				id={inputID}
+				placeholder={placeholderText}
+				className='py-3 px-4 leading-6 rounded border border-[#CFCFCF] focus:outline-[#007298] w-full'
+				onChange={onChange}
+			/>
+		</>
+	);
 };
