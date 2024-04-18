ab3eaf9724500e5ac1f95142a2981e5f83ac3a2e
asselzhl <asselzhly@gmail.com>
2024-04-18 20:19:37 +0500

    refactor getAuthorsName function

--- src/helpers/getAuthorsName.ts
+++ src/helpers/getAuthorsName.ts
@@ -3,7 +3,10 @@ interface AuthorInfo {
 	name: string;
 }
 
-export function getAuthorsName(authorsIDs: string[], authorsList: AuthorInfo[]) {
+export function getAuthorsName(
+	authorsIDs: string[],
+	authorsList: AuthorInfo[]
+) {
 	return authorsIDs
 		.map((authorID: string) => {
 			return authorsList.find((author) => author.id === authorID).name;
