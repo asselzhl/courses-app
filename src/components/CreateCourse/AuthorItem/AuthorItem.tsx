bc7a228a1c68e42392cec089789c90e8209c949d
asselzhl <asselzhly@gmail.com>
2024-04-18 20:00:27 +0500

    implement create course component

--- src/components/CreateCourse/AuthorItem/AuthorItem.tsx
+++ src/components/CreateCourse/AuthorItem/AuthorItem.tsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import addButton from './assets/add.svg';
-import deleteButton from './assets/delete.svg';
+import addButton from "./assets/add.svg";
+import deleteButton from "./assets/delete.svg";
 
-export const AuthorItem = ({ id, authorName, getAuthorInfo }) => {
-	const handleButtonClick = () => {
-		getAuthorInfo(id);
-	};
-	return (
-		<div className='flex gap-x-3 items-center'>
-			<p className='min-w-[150px]'>{authorName}</p>
-			<button onClick={handleButtonClick}>
-				<img src={addButton} alt='' />
-			</button>
-			<button onClick={handleButtonClick}>
-				<img src={deleteButton} alt='' />
-			</button>
-		</div>
-	);
+type AuthorItemKeys = "add" | "delete";
+
+const authorItemMap: Record<AuthorItemKeys, { src: string }> = {
+  add: {
+    src: addButton,
+  },
+  delete: {
+    src: deleteButton,
+  },
+};
+
+interface AuthorItemProps {
+  id: string;
+  authorName: string;
+  type?: AuthorItemKeys;
+  editAuthorList: (id: string) => void;
+}
+
+export const AuthorItem = ({
+  id,
+  authorName,
+  type = "add",
+  editAuthorList,
+}: AuthorItemProps) => {
+  const config = authorItemMap[type];
+
+  const handleButtonClick = () => {
+    editAuthorList(id);
+  };
+  return (
+    <div className="flex gap-x-3 items-center">
+      <p className="min-w-[150px]">{authorName}</p>
+      <button onClick={handleButtonClick}>
+        <img src={config.src} alt="" />
+      </button>
+    </div>
+  );
 };
