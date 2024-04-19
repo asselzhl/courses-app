a3c707d662b39e795e75a60e7819565e0e892617
asselzhl <asselzhly@gmail.com>
2024-04-19 15:38:25 +0500

    refactor header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Logo } from './components/Logo/Logo';
 import { Button } from '../../common/Button/Button';
 
 const style = {
-	header: `container mx-auto flex justify-between items-center min-h-20`
+	header: `container mx-auto flex justify-between items-center min-h-20`,
 };
 
 export const Header = () => {
