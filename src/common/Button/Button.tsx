026459ae2e27db16a521b586a540e70e2aeed522
asselzhl <asselzhly@gmail.com>
2024-04-28 13:57:15 +0500

    implement Create Course component validation

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
 
 const style = {
-  pageButton: `uppercase bg-[#007298] text-white rounded py-[13px] px-9 hover:opacity-80 duration-300`,
+	pageButton: `uppercase bg-[#007298] text-white rounded py-[13px] px-9 hover:opacity-80 duration-300`,
 };
 
-type ButtonType = "submit" | "reset" | "button";
+type ButtonType = 'submit' | 'reset' | 'button';
 interface ButtonProps {
-  text: string;
-  type?: ButtonType;
-  onClick: () => void;
+	text: string;
+	type?: ButtonType;
+	onClick: () => void;
 }
 
-export const Button = ({ text, type = "button", onClick }: ButtonProps) => {
-  return (
-    <button className={style.pageButton} onClick={onClick} type={type}>
-      {text}
-    </button>
-  );
+export const Button = ({ text, type = 'button', onClick }: ButtonProps) => {
+	return (
+		<button className={style.pageButton} onClick={onClick} type={type}>
+			{text}
+		</button>
+	);
 };
