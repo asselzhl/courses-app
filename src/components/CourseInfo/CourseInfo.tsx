50f221bd2d806ad2e46fcd16f0d01bfb5b1e6d57
asselzhl <asselzhly@gmail.com>
2024-05-01 18:30:43 +0500

    Implement a new feature for Header component

--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -1,83 +1,83 @@
-import React from "react";
+import React from 'react';
 
-import { Button } from "../../common/Button/Button";
-import { useNavigate, useParams } from "react-router-dom";
+import { Button } from '../../common/Button/Button';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { convertDateToDotFormat } from "../../helpers/convertDateToDotFormat";
-import { getCourseDuration } from "../../helpers/getCourseDuration";
-import { getAuthorsName } from "../../helpers/getAuthorsName";
+import { convertDateToDotFormat } from '../../helpers/convertDateToDotFormat';
+import { getCourseDuration } from '../../helpers/getCourseDuration';
+import { getAuthorsName } from '../../helpers/getAuthorsName';
 
 const style = {
-  title: `text-[#333E48] font-bold text-3xl mb-6`,
-  body: `border-[#CFCFCF] border bg-white py-14 px-16 rounded mb-12`,
-  subtitle: `text-[#333E48] text-xl font-bold mb-6`,
-  line: `w-[1px] bg-[#CFCFCF]`,
-  info: `w-[50%] flex flex-col gap-y-4`,
-  content: `flex gap-x-8`,
-  infoText: `flex justify-between text-[#333E48]`,
-  courseInfoWrapper: `bg-[#F7F7F7] h-screen py-20 px-40 flex flex-col gap-y-8`,
+	title: `text-[#333E48] font-bold text-3xl mb-6`,
+	body: `border-[#CFCFCF] border bg-white py-14 px-16 rounded mb-12`,
+	subtitle: `text-[#333E48] text-xl font-bold mb-6`,
+	line: `w-[1px] bg-[#CFCFCF]`,
+	info: `w-[50%] flex flex-col gap-y-4`,
+	content: `flex gap-x-8`,
+	infoText: `flex justify-between text-[#333E48]`,
+	courseInfoWrapper: `bg-[#F7F7F7] h-screen py-20 px-40 flex flex-col gap-y-8`,
 };
 
 interface AuthorsListItem {
-  id: string;
-  name: string;
+	id: string;
+	name: string;
 }
 interface CoursesListItem {
-  id: string;
-  title: string;
-  description: string;
-  creationDate: string;
-  duration: number;
-  authors: string[];
+	id: string;
+	title: string;
+	description: string;
+	creationDate: string;
+	duration: number;
+	authors: string[];
 }
 interface CourseInfoProps {
-  authorsList: AuthorsListItem[];
-  coursesList: CoursesListItem[];
+	authorsList: AuthorsListItem[];
+	coursesList: CoursesListItem[];
 }
 
 export const CourseInfo = ({ authorsList, coursesList }: CourseInfoProps) => {
-  const { courseId } = useParams();
-  const navigate = useNavigate();
+	const { courseId } = useParams();
+	const navigate = useNavigate();
 
-  const course = coursesList.find((course) => course.id === courseId);
-  const courseDuration = getCourseDuration(course.duration);
-  const courseCreationDate = convertDateToDotFormat(course.creationDate);
-  const courseAuthors = getAuthorsName(course.authors, authorsList);
+	const course = coursesList.find((course) => course.id === courseId);
+	const courseDuration = getCourseDuration(course.duration);
+	const courseCreationDate = convertDateToDotFormat(course.creationDate);
+	const courseAuthors = getAuthorsName(course.authors, authorsList);
 
-  const config = [
-    { title: "ID:", value: course.id },
-    { title: "Duration:", value: courseDuration },
-    { title: "Created:", value: courseCreationDate },
-    { title: "Authors:", value: courseAuthors },
-  ];
+	const config = [
+		{ title: 'ID:', value: course.id },
+		{ title: 'Duration:', value: courseDuration },
+		{ title: 'Created:', value: courseCreationDate },
+		{ title: 'Authors:', value: courseAuthors },
+	];
 
-  const handleButtonClick = () => {
-    navigate(-1);
-  };
+	const handleButtonClick = () => {
+		navigate(-1);
+	};
 
-  return (
-    <div className={style.courseInfoWrapper}>
-      <h2 className={style.title}>{course.title}</h2>
-      <div className={style.body}>
-        <h3 className={style.subtitle}>Description:</h3>
-        <div className={style.content}>
-          <div className="w-[50%]">
-            <p>{course.description}</p>
-          </div>
-          <div className={style.line}></div>
-          <div className={style.info}>
-            {config.map((infoItem, index) => (
-              <p className={style.infoText} key={index}>
-                <span className="font-bold">{infoItem.title}</span>
-                <span>{infoItem.value}</span>
-              </p>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className="text-right">
-        <Button text="back" onClick={handleButtonClick} />
-      </div>
-    </div>
-  );
+	return (
+		<div className={style.courseInfoWrapper}>
+			<h2 className={style.title}>{course.title}</h2>
+			<div className={style.body}>
+				<h3 className={style.subtitle}>Description:</h3>
+				<div className={style.content}>
+					<div className='w-[50%]'>
+						<p>{course.description}</p>
+					</div>
+					<div className={style.line}></div>
+					<div className={style.info}>
+						{config.map((infoItem, index) => (
+							<p className={style.infoText} key={index}>
+								<span className='font-bold'>{infoItem.title}</span>
+								<span>{infoItem.value}</span>
+							</p>
+						))}
+					</div>
+				</div>
+			</div>
+			<div className='text-right'>
+				<Button text='back' onClick={handleButtonClick} />
+			</div>
+		</div>
+	);
 };
