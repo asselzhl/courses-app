f74cd823686696faedd2a4e1ed0a201718c3c6ff
asselzhl <asselzhly@gmail.com>
2024-05-13 21:27:45 +0500

    implement course creation logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,49 +1,49 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { Logo } from "./components/Logo/Logo";
-import { Button } from "../../common/Button/Button";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "src/store";
+import { Logo } from './components/Logo/Logo';
+import { Button } from '../../common/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from 'src/store';
 
-import { logUserOut } from "../../store/user/userSlice";
+import { logUserOut } from '../../store/user/userSlice';
 
 const style = {
-  header: `container mx-auto flex justify-between items-center min-h-20`,
-  content: `flex items-center gap-x-3 font-bold`,
+	header: `container mx-auto flex justify-between items-center min-h-20`,
+	content: `flex items-center gap-x-3 font-bold`,
 };
 
 export const Header = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
+	const dispatch = useDispatch<AppDispatch>();
 
-  const userData = useSelector((state: RootState) => state.user);
+	const userData = useSelector((state: RootState) => state.user);
 
-  const handleButtonClick = () => {
-    // createRequest("http://localhost:4000/logout", "DELETE").then((response) => {
-    //   if (response.successful) {
-    //     localStorage.clear();
-    //     dispatch(logUserOut());
+	const handleButtonClick = () => {
+		// createRequest("http://localhost:4000/logout", "DELETE").then((response) => {
+		//   if (response.successful) {
+		//     localStorage.clear();
+		//     dispatch(logUserOut());
 
-    //     navigate("/login");
-    //   }
-    // });
+		//     navigate("/login");
+		//   }
+		// });
 
-    dispatch(logUserOut());
-    localStorage.clear();
+		dispatch(logUserOut());
+		localStorage.clear();
 
-    navigate("/login");
-  };
+		navigate('/login');
+	};
 
-  return (
-    <header className={style.header}>
-      <Logo />
-      <div className={style.content}>
-        {localStorage.getItem("userToken") && <p>{userData.name}</p>}
-        {localStorage.getItem("userToken") && (
-          <Button text="logout" onClick={handleButtonClick} />
-        )}
-      </div>
-    </header>
-  );
+	return (
+		<header className={style.header}>
+			<Logo />
+			<div className={style.content}>
+				{localStorage.getItem('userToken') && <p>{userData.name}</p>}
+				{localStorage.getItem('userToken') && (
+					<Button text='logout' onClick={handleButtonClick} />
+				)}
+			</div>
+		</header>
+	);
 };
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,94 +1,94 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-import { FormFieldWithError } from "../../common/FormFieldWithError/FormFieldWithError";
-import { Button } from "../../common/Button/Button";
+import { FormFieldWithError } from '../../common/FormFieldWithError/FormFieldWithError';
+import { Button } from '../../common/Button/Button';
 
-import { validateInputValues } from "../../helpers/validateInputValues";
+import { validateInputValues } from '../../helpers/validateInputValues';
 
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "src/store";
-import { authenticateUser } from "../../store/user/userSlice";
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from 'src/store';
+import { authenticateUser } from '../../store/user/userSlice';
 
 const style = {
-  blockTitle: `text-[#333E48] font-bold text-3xl mb-6`,
-  formContainer: `border-[#CFCFCF] border bg-white rounded w-[600px] py-20 px-36 flex flex-col gap-y-8`,
-  loginFormWrapper: `bg-[#F7F7F7] h-screen py-20 flex flex-col justify-center items-center`,
+	blockTitle: `text-[#333E48] font-bold text-3xl mb-6`,
+	formContainer: `border-[#CFCFCF] border bg-white rounded w-[600px] py-20 px-36 flex flex-col gap-y-8`,
+	loginFormWrapper: `bg-[#F7F7F7] h-screen py-20 flex flex-col justify-center items-center`,
 };
 
 interface ErrorMessages {
-  [key: string]: string;
+	[key: string]: string;
 }
 
 export const Login = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [errorMessages, setErrorMessages] = useState<ErrorMessages>({});
-  const initialUserData = {
-    email: "",
-    password: "",
-  };
+	const [errorMessages, setErrorMessages] = useState<ErrorMessages>({});
+	const initialUserData = {
+		email: '',
+		password: '',
+	};
 
-  const [userData, setUserData] = useState(initialUserData);
+	const [userData, setUserData] = useState(initialUserData);
 
-  const dispatch = useDispatch<AppDispatch>();
+	const dispatch = useDispatch<AppDispatch>();
 
-  const handleUserDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserData((prevValues) => {
-      return { ...prevValues, [e.target.name]: e.target.value };
-    });
-  };
+	const handleUserDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUserData((prevValues) => {
+			return { ...prevValues, [e.target.name]: e.target.value };
+		});
+	};
 
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+	const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-    const errors = validateInputValues(userData, {});
+		const errors = validateInputValues(userData, {});
 
-    setErrorMessages(errors);
+		setErrorMessages(errors);
 
-    if (Object.keys(errors).length === 0) {
-      dispatch(authenticateUser(userData));
-      navigate("/courses");
-    }
-  };
-  return (
-    <>
-      <div className={style.loginFormWrapper}>
-        <h2 className={style.blockTitle}>Login</h2>
+		if (Object.keys(errors).length === 0) {
+			dispatch(authenticateUser(userData));
+			navigate('/courses');
+		}
+	};
+	return (
+		<>
+			<div className={style.loginFormWrapper}>
+				<h2 className={style.blockTitle}>Login</h2>
 
-        <form className={style.formContainer} onSubmit={handleFormSubmit}>
-          <FormFieldWithError
-            type="email"
-            labelText="Email"
-            name="email"
-            placeholderText="Email"
-            value={userData.email}
-            inputID="email"
-            errorMessage={errorMessages.email}
-            onChange={handleUserDataChange}
-          />
+				<form className={style.formContainer} onSubmit={handleFormSubmit}>
+					<FormFieldWithError
+						type='email'
+						labelText='Email'
+						name='email'
+						placeholderText='Email'
+						value={userData.email}
+						inputID='email'
+						errorMessage={errorMessages.email}
+						onChange={handleUserDataChange}
+					/>
 
-          <FormFieldWithError
-            type="password"
-            labelText="Password"
-            name="password"
-            placeholderText="Password"
-            value={userData.password}
-            inputID="password"
-            errorMessage={errorMessages.password}
-            onChange={handleUserDataChange}
-          />
+					<FormFieldWithError
+						type='password'
+						labelText='Password'
+						name='password'
+						placeholderText='Password'
+						value={userData.password}
+						inputID='password'
+						errorMessage={errorMessages.password}
+						onChange={handleUserDataChange}
+					/>
 
-          <Button text="login" type="submit" onClick={() => {}} />
+					<Button text='login' type='submit' onClick={() => {}} />
 
-          <div className="text-center">
-            <span>If you have an account you may </span>
-            <Link className="font-bold" to="/registration">
-              Registration
-            </Link>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+					<div className='text-center'>
+						<span>If you have an account you may </span>
+						<Link className='font-bold' to='/registration'>
+							Registration
+						</Link>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 };
