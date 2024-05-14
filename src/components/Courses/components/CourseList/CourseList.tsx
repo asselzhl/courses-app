06f3000549d6329c32a52ac7a5ca18d89f6669b4
asselzhl <asselzhly@gmail.com>
2024-05-14 16:04:03 +0500

    fix delete course bug

--- src/components/Courses/components/CourseList/CourseList.tsx
+++ src/components/Courses/components/CourseList/CourseList.tsx
@@ -1,52 +1,52 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "src/store";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from 'src/store';
 
-import { convertDateToDotFormat } from "../../../../helpers/convertDateToDotFormat";
-import { getCourseDuration } from "../../../../helpers/getCourseDuration";
-import { getAuthorsName } from "../../../../helpers/getAuthorsName";
+import { convertDateToDotFormat } from '../../../../helpers/convertDateToDotFormat';
+import { getCourseDuration } from '../../../../helpers/getCourseDuration';
+import { getAuthorsName } from '../../../../helpers/getAuthorsName';
 
-import { fetchAuthors } from "../../../../store/authors/authorsSlice";
+import { fetchAuthors } from '../../../../store/authors/authorsSlice';
 
-import { CourseCard } from "../CourseCard/CourseCard";
+import { CourseCard } from '../CourseCard/CourseCard';
 
 export const CourseList = () => {
-  const authorsList = useSelector((state: RootState) => state.authors.data);
-  const filteredCourses = useSelector(
-    (state: RootState) => state.courses.filteredCourses
-  );
+	const authorsList = useSelector((state: RootState) => state.authors.data);
+	const filteredCourses = useSelector(
+		(state: RootState) => state.courses.filteredCourses
+	);
 
-  const authorsStatus = useSelector((state: RootState) => state.authors.status);
-  const coursesStatus = useSelector((state: RootState) => state.courses.status);
-  const dispatch = useDispatch<AppDispatch>();
+	const authorsStatus = useSelector((state: RootState) => state.authors.status);
+	const coursesStatus = useSelector((state: RootState) => state.courses.status);
+	const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(() => {
-    if (authorsStatus === "idle") {
-      dispatch(fetchAuthors());
-    }
-  }, [authorsStatus, dispatch]);
+	useEffect(() => {
+		if (authorsStatus === 'idle') {
+			dispatch(fetchAuthors());
+		}
+	}, [authorsStatus, dispatch]);
 
-  if (authorsStatus === "succeeded" && coursesStatus === "succeeded") {
-    return (
-      <ul>
-        {filteredCourses.map((course) => {
-          const authors = getAuthorsName(course.authors, authorsList);
-          const creationDate = convertDateToDotFormat(course.creationDate);
-          const duration = getCourseDuration(course.duration);
+	if (authorsStatus === 'succeeded' && coursesStatus === 'succeeded') {
+		return (
+			<ul>
+				{filteredCourses.map((course) => {
+					const authors = getAuthorsName(course.authors, authorsList);
+					const creationDate = convertDateToDotFormat(course.creationDate);
+					const duration = getCourseDuration(course.duration);
 
-          return (
-            <CourseCard
-              key={course.id}
-              id={course.id}
-              courseName={course.title}
-              duration={duration}
-              creationDate={creationDate}
-              description={course.description}
-              authors={authors}
-            />
-          );
-        })}
-      </ul>
-    );
-  }
+					return (
+						<CourseCard
+							key={course.id}
+							id={course.id}
+							courseName={course.title}
+							duration={duration}
+							creationDate={creationDate}
+							description={course.description}
+							authors={authors}
+						/>
+					);
+				})}
+			</ul>
+		);
+	}
 };
