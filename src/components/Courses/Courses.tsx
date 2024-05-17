099d411aabdf098b5ced684fd04fc1aa9d83333d
asselzhl <asselzhly@gmail.com>
2024-05-17 11:33:21 +0500

    implement private route component

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,68 +1,68 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-import { EmptyCourseList } from "../EmptyCourseList/EmptyCourseList";
-import { SearchBar } from "./components/SearchBar/SearchBar";
-import { Button } from "../../common/Button/Button";
+import { EmptyCourseList } from '../EmptyCourseList/EmptyCourseList';
+import { SearchBar } from './components/SearchBar/SearchBar';
+import { Button } from '../../common/Button/Button';
 
-import { CourseList } from "./components/CourseList/CourseList";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "src/store";
-import { fetchCourses } from "../../store/courses/coursesSlice";
-import { getCoursesData, getUserData } from "../../store/selectors";
+import { CourseList } from './components/CourseList/CourseList';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from 'src/store';
+import { fetchCourses } from '../../store/courses/coursesSlice';
+import { getCoursesData, getUserData } from '../../store/selectors';
 
 const style = {
-  coursesListWrapper: `bg-[#F7F7F7] h-full py-20 px-40 flex flex-col gap-y-8`,
-  emptyCourseListWrapper: `bg-[#F7F7F7] h-[90vh] py-20 px-40 flex flex-col gap-y-8 justify-center items-center`,
+	coursesListWrapper: `bg-[#F7F7F7] h-full py-20 px-40 flex flex-col gap-y-8`,
+	emptyCourseListWrapper: `bg-[#F7F7F7] h-[90vh] py-20 px-40 flex flex-col gap-y-8 justify-center items-center`,
 };
 
 export const Courses = () => {
-  // const [searchValue, setSearchValue] = useState<string>("");
-  const dispatch = useDispatch<AppDispatch>();
+	// const [searchValue, setSearchValue] = useState<string>("");
+	const dispatch = useDispatch<AppDispatch>();
 
-  const coursesData = useSelector(getCoursesData);
+	const coursesData = useSelector(getCoursesData);
 
-  const coursesStatus = coursesData.status;
+	const coursesStatus = coursesData.status;
 
-  const userData = useSelector(getUserData);
+	const userData = useSelector(getUserData);
 
-  useEffect(() => {
-    dispatch(fetchCourses());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchCourses());
+	}, [dispatch]);
 
-  // useEffect(() => {
-  //   dispatch(searchCourse(searchValue));
-  // }, [coursesList, dispatch]);
+	// useEffect(() => {
+	//   dispatch(searchCourse(searchValue));
+	// }, [coursesList, dispatch]);
 
-  // const handleSearchButtonClick = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   dispatch(searchCourse(searchValue));
-  // };
+	// const handleSearchButtonClick = (e: React.FormEvent<HTMLFormElement>) => {
+	//   e.preventDefault();
+	//   dispatch(searchCourse(searchValue));
+	// };
 
-  if (coursesStatus === "succeeded") {
-    return (
-      <>
-        <div className={style.coursesListWrapper}>
-          <div className="flex justify-between">
-            <SearchBar />
-            {userData.role === "admin" ? (
-              <Link to="/courses/add">
-                <Button text="Add new course" onClick={() => {}} />
-              </Link>
-            ) : null}
-          </div>
+	if (coursesStatus === 'succeeded') {
+		return (
+			<>
+				<div className={style.coursesListWrapper}>
+					<div className='flex justify-between'>
+						<SearchBar />
+						{userData.role === 'admin' ? (
+							<Link to='/courses/add'>
+								<Button text='Add new course' onClick={() => {}} />
+							</Link>
+						) : null}
+					</div>
 
-          <CourseList />
-        </div>
-      </>
-    );
-  }
+					<CourseList />
+				</div>
+			</>
+		);
+	}
 
-  return (
-    <>
-      <div className={style.emptyCourseListWrapper}>
-        <EmptyCourseList />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={style.emptyCourseListWrapper}>
+				<EmptyCourseList />
+			</div>
+		</>
+	);
 };
--- src/components/Courses/components/CourseList/CourseList.tsx
+++ src/components/Courses/components/CourseList/CourseList.tsx
@@ -1,58 +1,58 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "src/store";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from 'src/store';
 
-import { convertDateToDotFormat } from "../../../../helpers/convertDateToDotFormat";
-import { getCourseDuration } from "../../../../helpers/getCourseDuration";
-import { getAuthorsName } from "../../../../helpers/getAuthorsName";
+import { convertDateToDotFormat } from '../../../../helpers/convertDateToDotFormat';
+import { getCourseDuration } from '../../../../helpers/getCourseDuration';
+import { getAuthorsName } from '../../../../helpers/getAuthorsName';
 
-import { fetchAuthors } from "../../../../store/authors/authorsSlice";
+import { fetchAuthors } from '../../../../store/authors/authorsSlice';
 
-import { CourseCard } from "../CourseCard/CourseCard";
+import { CourseCard } from '../CourseCard/CourseCard';
 import {
-  getAuthorsData,
-  getCoursesData,
-  getFilteredCourses,
-} from "../../../../store/selectors";
+	getAuthorsData,
+	getCoursesData,
+	getFilteredCourses,
+} from '../../../../store/selectors';
 
 export const CourseList = () => {
-  const coursesData = useSelector(getCoursesData);
+	const coursesData = useSelector(getCoursesData);
 
-  const authorsData = useSelector(getAuthorsData);
-  const authorsList = authorsData.data;
-  const filteredCourses = useSelector(getFilteredCourses);
+	const authorsData = useSelector(getAuthorsData);
+	const authorsList = authorsData.data;
+	const filteredCourses = useSelector(getFilteredCourses);
 
-  const authorsStatus = authorsData.status;
-  const coursesStatus = coursesData.status;
-  const dispatch = useDispatch<AppDispatch>();
+	const authorsStatus = authorsData.status;
+	const coursesStatus = coursesData.status;
+	const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(() => {
-    if (authorsStatus === "idle") {
-      dispatch(fetchAuthors());
-    }
-  }, [authorsStatus, dispatch]);
+	useEffect(() => {
+		if (authorsStatus === 'idle') {
+			dispatch(fetchAuthors());
+		}
+	}, [authorsStatus, dispatch]);
 
-  if (authorsStatus === "succeeded" && coursesStatus === "succeeded") {
-    return (
-      <ul>
-        {filteredCourses.map((course) => {
-          const authors = getAuthorsName(course.authors, authorsList);
-          const creationDate = convertDateToDotFormat(course.creationDate);
-          const duration = getCourseDuration(course.duration);
+	if (authorsStatus === 'succeeded' && coursesStatus === 'succeeded') {
+		return (
+			<ul>
+				{filteredCourses.map((course) => {
+					const authors = getAuthorsName(course.authors, authorsList);
+					const creationDate = convertDateToDotFormat(course.creationDate);
+					const duration = getCourseDuration(course.duration);
 
-          return (
-            <CourseCard
-              key={course.id}
-              id={course.id}
-              courseName={course.title}
-              duration={duration}
-              creationDate={creationDate}
-              description={course.description}
-              authors={authors}
-            />
-          );
-        })}
-      </ul>
-    );
-  }
+					return (
+						<CourseCard
+							key={course.id}
+							id={course.id}
+							courseName={course.title}
+							duration={duration}
+							creationDate={creationDate}
+							description={course.description}
+							authors={authors}
+						/>
+					);
+				})}
+			</ul>
+		);
+	}
 };
