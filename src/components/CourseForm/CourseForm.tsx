0b42b33e34a3e585ae199e8242ac2bdd5d126e0a
asselzhl <asselzhly@gmail.com>
2024-05-18 16:43:19 +0500

    rewrite authors requests with axios

--- src/components/CourseForm/CourseForm.tsx
+++ src/components/CourseForm/CourseForm.tsx
@@ -1,282 +1,282 @@
-import React, { useEffect, useState } from "react";
-import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "src/store";
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from 'src/store';
 
-import { FormFieldWithError } from "../../common/FormFieldWithError/FormFieldWithError";
-import { Button } from "../../common/Button/Button";
-import { TextareaWithError } from "../../common/TextareaWithError/TextareaWithError";
-import { ErrorMessage } from "../../common/ErrorMessage/ErrorMessage";
-import { AuthorItem } from "./AuthorItem/AuthorItem";
+import { FormFieldWithError } from '../../common/FormFieldWithError/FormFieldWithError';
+import { Button } from '../../common/Button/Button';
+import { TextareaWithError } from '../../common/TextareaWithError/TextareaWithError';
+import { ErrorMessage } from '../../common/ErrorMessage/ErrorMessage';
+import { AuthorItem } from './AuthorItem/AuthorItem';
 
-import { getCourseDuration } from "../../helpers/getCourseDuration";
-import { validateInputValues } from "../../helpers/validateInputValues";
+import { getCourseDuration } from '../../helpers/getCourseDuration';
+import { validateInputValues } from '../../helpers/validateInputValues';
 import {
-  addAuthor,
-  fetchAuthors,
-  addCourse,
-  updateCourse,
-} from "../../store/operations";
-import { getAuthorsData, getCoursesData } from "../../store/selectors";
+	addAuthor,
+	fetchAuthors,
+	addCourse,
+	updateCourse,
+} from '../../store/operations';
+import { getAuthorsData, getCoursesData } from '../../store/selectors';
 
 const style = {
-  sectionTitle: `text-[#333E48] font-bold text-3xl mb-6 capitalize`,
-  section: `border-[#CFCFCF] border bg-white rounded py-10 px-20 flex flex-col gap-y-8 mb-8`,
-  sectionSubtitle: `text-2xl font-bold`,
-  errorMessage: `text-[#FF0000]`,
-  durationContainer: `flex items-center gap-x-4`,
-  availableAuthorsTitle: `text-2xl font-bold mb-6`,
-  newCourseAuthorsContainer: `max-w-[20%] flex flex-col gap-y-4 text-center`,
-  createCourseWrapper: `bg-[#F7F7F7] h-full py-20 px-40 flex flex-col`,
+	sectionTitle: `text-[#333E48] font-bold text-3xl mb-6 capitalize`,
+	section: `border-[#CFCFCF] border bg-white rounded py-10 px-20 flex flex-col gap-y-8 mb-8`,
+	sectionSubtitle: `text-2xl font-bold`,
+	errorMessage: `text-[#FF0000]`,
+	durationContainer: `flex items-center gap-x-4`,
+	availableAuthorsTitle: `text-2xl font-bold mb-6`,
+	newCourseAuthorsContainer: `max-w-[20%] flex flex-col gap-y-4 text-center`,
+	createCourseWrapper: `bg-[#F7F7F7] h-full py-20 px-40 flex flex-col`,
 };
 
 interface ErrorMessages {
-  [key: string]: string;
+	[key: string]: string;
 }
 
 interface CoursesListItem {
-  title: string;
-  description: string;
-  duration;
-  authors: string[];
+	title: string;
+	description: string;
+	duration;
+	authors: string[];
 }
 interface AuthorsListItem {
-  id: string;
-  name: string;
+	id: string;
+	name: string;
 }
 
 export const CreateCourse = () => {
-  const { courseId } = useParams();
-  const location = useLocation();
+	const { courseId } = useParams();
+	const location = useLocation();
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const coursesData = useSelector(getCoursesData);
-  const coursesList = coursesData.data;
+	const coursesData = useSelector(getCoursesData);
+	const coursesList = coursesData.data;
 
-  const authorsData = useSelector(getAuthorsData);
-  const authorsList = authorsData.data;
-  const authorsStatus = authorsData.status;
+	const authorsData = useSelector(getAuthorsData);
+	const authorsList = authorsData.data;
+	const authorsStatus = authorsData.status;
 
-  const dispatch = useDispatch<AppDispatch>();
+	const dispatch = useDispatch<AppDispatch>();
 
-  const initialNewCourseData: CoursesListItem = {
-    title: "",
-    description: "",
-    duration: "",
-    authors: [],
-  };
+	const initialNewCourseData: CoursesListItem = {
+		title: '',
+		description: '',
+		duration: '',
+		authors: [],
+	};
 
-  let courseAuthorsData: AuthorsListItem[] = [];
-  if (courseId) {
-    const course = coursesList.find((course) => course.id === courseId);
-    initialNewCourseData.title = course.title;
-    initialNewCourseData.description = course.description;
-    initialNewCourseData.duration = course.duration;
-    initialNewCourseData.authors = course.authors;
+	let courseAuthorsData: AuthorsListItem[] = [];
+	if (courseId) {
+		const course = coursesList.find((course) => course.id === courseId);
+		initialNewCourseData.title = course.title;
+		initialNewCourseData.description = course.description;
+		initialNewCourseData.duration = course.duration;
+		initialNewCourseData.authors = course.authors;
 
-    courseAuthorsData = initialNewCourseData.authors.map((authorID) => {
-      const authorName = authorsList.find(
-        (authorData) => authorData.id === authorID
-      ).name;
-      return { id: authorID, name: authorName };
-    });
-  }
+		courseAuthorsData = initialNewCourseData.authors.map((authorID) => {
+			const authorName = authorsList.find(
+				(authorData) => authorData.id === authorID
+			).name;
+			return { id: authorID, name: authorName };
+		});
+	}
 
-  useEffect(() => {
-    if (authorsStatus === "idle") {
-      dispatch(fetchAuthors());
-    }
-  }, [authorsStatus, dispatch]);
+	useEffect(() => {
+		if (authorsStatus === 'idle') {
+			dispatch(fetchAuthors());
+		}
+	}, [authorsStatus, dispatch]);
 
-  useEffect(() => {
-    dispatch(fetchAuthors());
-  }, [authorsList, dispatch]);
+	useEffect(() => {
+		dispatch(fetchAuthors());
+	}, [authorsList, dispatch]);
 
-  const [newCourseAuthors, setNewCourseAuthors] =
-    useState<AuthorsListItem[]>(courseAuthorsData);
+	const [newCourseAuthors, setNewCourseAuthors] =
+		useState<AuthorsListItem[]>(courseAuthorsData);
 
-  const [newAuthor, setNewAuthor] = useState<string>("");
+	const [newAuthor, setNewAuthor] = useState<string>('');
 
-  const [newCourseData, setNewCourseData] =
-    useState<CoursesListItem>(initialNewCourseData);
+	const [newCourseData, setNewCourseData] =
+		useState<CoursesListItem>(initialNewCourseData);
 
-  const [errorMessages, setErrorMessages] = useState<ErrorMessages>({});
+	const [errorMessages, setErrorMessages] = useState<ErrorMessages>({});
 
-  const handleNewCourseDataChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setNewCourseData((prevValues) => {
-      return { ...prevValues, [e.target.name]: e.target.value };
-    });
-  };
+	const handleNewCourseDataChange = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNewCourseData((prevValues) => {
+			return { ...prevValues, [e.target.name]: e.target.value };
+		});
+	};
 
-  const addAuthorName = (authorID: string): void => {
-    const author = authorsList.find((author) => author.id === authorID);
+	const addAuthorName = (authorID: string): void => {
+		const author = authorsList.find((author) => author.id === authorID);
 
-    setNewCourseAuthors([...newCourseAuthors, author]);
+		setNewCourseAuthors([...newCourseAuthors, author]);
 
-    setNewCourseData((prevValues) => {
-      return {
-        ...prevValues,
-        authors: [...newCourseAuthors.map((author) => author.id), author.id],
-      };
-    });
-  };
+		setNewCourseData((prevValues) => {
+			return {
+				...prevValues,
+				authors: [...newCourseAuthors.map((author) => author.id), author.id],
+			};
+		});
+	};
 
-  const deleteAuthorName = (authorID: string): void => {
-    const removedAuthors = newCourseAuthors.filter(
-      (author) => author.id !== authorID
-    );
+	const deleteAuthorName = (authorID: string): void => {
+		const removedAuthors = newCourseAuthors.filter(
+			(author) => author.id !== authorID
+		);
 
-    setNewCourseAuthors(removedAuthors);
-    setNewCourseData((prevValues) => {
-      return {
-        ...prevValues,
-        authors: removedAuthors.map((author) => author.id),
-      };
-    });
-  };
+		setNewCourseAuthors(removedAuthors);
+		setNewCourseData((prevValues) => {
+			return {
+				...prevValues,
+				authors: removedAuthors.map((author) => author.id),
+			};
+		});
+	};
 
-  const createNewAuthor = (): void => {
-    const createdAuthorInfo = { name: newAuthor };
+	const createNewAuthor = (): void => {
+		const createdAuthorInfo = { name: newAuthor };
 
-    dispatch(addAuthor(createdAuthorInfo));
-    setNewAuthor("");
-  };
+		dispatch(addAuthor(createdAuthorInfo));
+		setNewAuthor('');
+	};
 
-  const handleFormSubmit = () => {
-    const errors = validateInputValues(newCourseData, {});
+	const handleFormSubmit = () => {
+		const errors = validateInputValues(newCourseData, {});
 
-    setErrorMessages(errors);
+		setErrorMessages(errors);
 
-    if (Object.keys(errors).length === 0) {
-      newCourseData.duration = Number(newCourseData.duration);
+		if (Object.keys(errors).length === 0) {
+			newCourseData.duration = Number(newCourseData.duration);
 
-      dispatch(addCourse(newCourseData));
-      navigate("/courses");
-    }
-  };
-  const handleUpdateButtonClick = () => {
-    newCourseData.duration = Number(newCourseData.duration);
-    const courseData = {
-      newCourseData: newCourseData,
-      courseId: courseId,
-    };
-    console.log(courseData);
-    dispatch(updateCourse(courseData));
-    navigate("/courses");
-  };
+			dispatch(addCourse(newCourseData));
+			navigate('/courses');
+		}
+	};
+	const handleUpdateButtonClick = () => {
+		newCourseData.duration = Number(newCourseData.duration);
+		const courseData = {
+			newCourseData: newCourseData,
+			courseId: courseId,
+		};
+		console.log(courseData);
+		dispatch(updateCourse(courseData));
+		navigate('/courses');
+	};
 
-  return (
-    <div className={style.createCourseWrapper}>
-      <h2 className={style.sectionTitle}>Course edit/create page</h2>
+	return (
+		<div className={style.createCourseWrapper}>
+			<h2 className={style.sectionTitle}>Course edit/create page</h2>
 
-      <form action="">
-        <div className={style.section}>
-          <h3 className={style.sectionSubtitle}>Main Info</h3>
+			<form action=''>
+				<div className={style.section}>
+					<h3 className={style.sectionSubtitle}>Main Info</h3>
 
-          <FormFieldWithError
-            type="text"
-            labelText="Title"
-            placeholderText="Course Titlte"
-            name="title"
-            value={newCourseData.title}
-            inputID="courseName"
-            errorMessage={errorMessages.title}
-            onChange={handleNewCourseDataChange}
-          />
+					<FormFieldWithError
+						type='text'
+						labelText='Title'
+						placeholderText='Course Titlte'
+						name='title'
+						value={newCourseData.title}
+						inputID='courseName'
+						errorMessage={errorMessages.title}
+						onChange={handleNewCourseDataChange}
+					/>
 
-          <TextareaWithError
-            labelText="Description"
-            placeholderText="Description"
-            name="description"
-            value={newCourseData.description}
-            textareaID="description"
-            errorMessage={errorMessages.description}
-            onChange={handleNewCourseDataChange}
-          />
+					<TextareaWithError
+						labelText='Description'
+						placeholderText='Description'
+						name='description'
+						value={newCourseData.description}
+						textareaID='description'
+						errorMessage={errorMessages.description}
+						onChange={handleNewCourseDataChange}
+					/>
 
-          <div className={style.durationContainer}>
-            <FormFieldWithError
-              type="number"
-              labelText="Duration"
-              placeholderText="Duration"
-              name="duration"
-              value={newCourseData.duration}
-              inputID="duration"
-              errorMessage={errorMessages.duration}
-              onChange={handleNewCourseDataChange}
-            />
+					<div className={style.durationContainer}>
+						<FormFieldWithError
+							type='number'
+							labelText='Duration'
+							placeholderText='Duration'
+							name='duration'
+							value={newCourseData.duration}
+							inputID='duration'
+							errorMessage={errorMessages.duration}
+							onChange={handleNewCourseDataChange}
+						/>
 
-            <p>
-              {newCourseData.duration
-                ? getCourseDuration(newCourseData.duration)
-                : "00:00 hours"}
-            </p>
-          </div>
-          <div className="flex justify-between">
-            <div>
-              <h3 className={style.availableAuthorsTitle}>Authors</h3>
-              <div className="flex items-end gap-x-4">
-                <FormFieldWithError
-                  type="text"
-                  labelText="Author Name"
-                  placeholderText="Author Name"
-                  name="author"
-                  value={newAuthor}
-                  inputID="author"
-                  onChange={(e) => setNewAuthor(e.target.value)}
-                />
-                <Button text="create author" onClick={createNewAuthor} />
-              </div>
-            </div>
+						<p>
+							{newCourseData.duration
+								? getCourseDuration(newCourseData.duration)
+								: '00:00 hours'}
+						</p>
+					</div>
+					<div className='flex justify-between'>
+						<div>
+							<h3 className={style.availableAuthorsTitle}>Authors</h3>
+							<div className='flex items-end gap-x-4'>
+								<FormFieldWithError
+									type='text'
+									labelText='Author Name'
+									placeholderText='Author Name'
+									name='author'
+									value={newAuthor}
+									inputID='author'
+									onChange={(e) => setNewAuthor(e.target.value)}
+								/>
+								<Button text='create author' onClick={createNewAuthor} />
+							</div>
+						</div>
 
-            <div className={style.newCourseAuthorsContainer}>
-              <h3 className="text-2xl font-bold">Course Authors</h3>
-              {newCourseAuthors.length
-                ? newCourseAuthors.map((authorName) => {
-                    return (
-                      <AuthorItem
-                        key={authorName.id}
-                        id={authorName.id}
-                        authorName={authorName.name}
-                        type="delete"
-                        editAuthorList={deleteAuthorName}
-                      />
-                    );
-                  })
-                : "Author list is empty"}
-              <ErrorMessage errorMessage={errorMessages.authors} />
-            </div>
-          </div>
+						<div className={style.newCourseAuthorsContainer}>
+							<h3 className='text-2xl font-bold'>Course Authors</h3>
+							{newCourseAuthors.length
+								? newCourseAuthors.map((authorName) => {
+										return (
+											<AuthorItem
+												key={authorName.id}
+												id={authorName.id}
+												authorName={authorName.name}
+												type='delete'
+												editAuthorList={deleteAuthorName}
+											/>
+										);
+									})
+								: 'Author list is empty'}
+							<ErrorMessage errorMessage={errorMessages.authors} />
+						</div>
+					</div>
 
-          <div className="flex flex-col gap-y-4">
-            <h4 className="font-bold">Authors List</h4>
-            {authorsList.map((author) => {
-              return (
-                <AuthorItem
-                  key={author.id}
-                  id={author.id}
-                  authorName={author.name}
-                  editAuthorList={addAuthorName}
-                />
-              );
-            })}
-          </div>
-        </div>
+					<div className='flex flex-col gap-y-4'>
+						<h4 className='font-bold'>Authors List</h4>
+						{authorsList.map((author) => {
+							return (
+								<AuthorItem
+									key={author.id}
+									id={author.id}
+									authorName={author.name}
+									editAuthorList={addAuthorName}
+								/>
+							);
+						})}
+					</div>
+				</div>
 
-        <div className="flex gap-x-5 justify-end">
-          <Link to="/courses">
-            <Button text="cancel" onClick={() => {}} />
-          </Link>
-          {location.pathname === "/courses/add" ? (
-            <Button text="create course" onClick={handleFormSubmit} />
-          ) : (
-            <Button text="update course" onClick={handleUpdateButtonClick} />
-          )}
-        </div>
-      </form>
-    </div>
-  );
+				<div className='flex gap-x-5 justify-end'>
+					<Link to='/courses'>
+						<Button text='cancel' onClick={() => {}} />
+					</Link>
+					{location.pathname === '/courses/add' ? (
+						<Button text='create course' onClick={handleFormSubmit} />
+					) : (
+						<Button text='update course' onClick={handleUpdateButtonClick} />
+					)}
+				</div>
+			</form>
+		</div>
+	);
 };
--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -1,94 +1,94 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import { Button } from "../../common/Button/Button";
-import { useNavigate, useParams } from "react-router-dom";
+import { Button } from '../../common/Button/Button';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { convertDateToDotFormat } from "../../helpers/convertDateToDotFormat";
-import { getCourseDuration } from "../../helpers/getCourseDuration";
-import { getAuthorsName } from "../../helpers/getAuthorsName";
-import { useSelector, useDispatch } from "react-redux";
-import { AppDispatch } from "src/store";
+import { convertDateToDotFormat } from '../../helpers/convertDateToDotFormat';
+import { getCourseDuration } from '../../helpers/getCourseDuration';
+import { getAuthorsName } from '../../helpers/getAuthorsName';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppDispatch } from 'src/store';
 
-import { fetchCourses, fetchAuthors } from "../../store/operations";
-import { getAuthorsData, getCoursesData } from "../../store/selectors";
+import { fetchCourses, fetchAuthors } from '../../store/operations';
+import { getAuthorsData, getCoursesData } from '../../store/selectors';
 
 const style = {
-  title: `text-[#333E48] font-bold text-3xl mb-6`,
-  body: `border-[#CFCFCF] border bg-white py-14 px-16 rounded mb-12`,
-  subtitle: `text-[#333E48] text-xl font-bold mb-6`,
-  line: `w-[1px] bg-[#CFCFCF]`,
-  info: `w-[50%] flex flex-col gap-y-4`,
-  content: `flex gap-x-8`,
-  infoText: `flex justify-between text-[#333E48]`,
-  courseInfoWrapper: `bg-[#F7F7F7] h-screen py-20 px-40 flex flex-col gap-y-8`,
+	title: `text-[#333E48] font-bold text-3xl mb-6`,
+	body: `border-[#CFCFCF] border bg-white py-14 px-16 rounded mb-12`,
+	subtitle: `text-[#333E48] text-xl font-bold mb-6`,
+	line: `w-[1px] bg-[#CFCFCF]`,
+	info: `w-[50%] flex flex-col gap-y-4`,
+	content: `flex gap-x-8`,
+	infoText: `flex justify-between text-[#333E48]`,
+	courseInfoWrapper: `bg-[#F7F7F7] h-screen py-20 px-40 flex flex-col gap-y-8`,
 };
 
 export const CourseInfo = () => {
-  const { courseId } = useParams();
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
+	const { courseId } = useParams();
+	const navigate = useNavigate();
+	const dispatch = useDispatch<AppDispatch>();
 
-  const coursesData = useSelector(getCoursesData);
+	const coursesData = useSelector(getCoursesData);
 
-  const coursesList = coursesData.data;
+	const coursesList = coursesData.data;
 
-  const authorsData = useSelector(getAuthorsData);
-  const authorsList = authorsData.data;
+	const authorsData = useSelector(getAuthorsData);
+	const authorsList = authorsData.data;
 
-  const coursesStatus = coursesData.status;
-  const authorsStatus = authorsData.status;
+	const coursesStatus = coursesData.status;
+	const authorsStatus = authorsData.status;
 
-  useEffect(() => {
-    if (coursesStatus === "idle" && authorsStatus === "idle") {
-      dispatch(fetchCourses());
-      dispatch(fetchAuthors());
-    }
-  }, [coursesStatus, authorsStatus, dispatch]);
+	useEffect(() => {
+		if (coursesStatus === 'idle' && authorsStatus === 'idle') {
+			dispatch(fetchCourses());
+			dispatch(fetchAuthors());
+		}
+	}, [coursesStatus, authorsStatus, dispatch]);
 
-  const handleButtonClick = () => {
-    navigate("/courses");
-  };
-  if (coursesStatus === "succeeded" && authorsStatus === "succeeded") {
-    const course = coursesList.find((course) => course.id === courseId);
-    const courseDuration = course ? getCourseDuration(course.duration) : "";
-    const courseCreationDate = course
-      ? convertDateToDotFormat(course.creationDate)
-      : "";
-    const courseAuthors = course
-      ? getAuthorsName(course.authors, authorsList)
-      : "";
+	const handleButtonClick = () => {
+		navigate('/courses');
+	};
+	if (coursesStatus === 'succeeded' && authorsStatus === 'succeeded') {
+		const course = coursesList.find((course) => course.id === courseId);
+		const courseDuration = course ? getCourseDuration(course.duration) : '';
+		const courseCreationDate = course
+			? convertDateToDotFormat(course.creationDate)
+			: '';
+		const courseAuthors = course
+			? getAuthorsName(course.authors, authorsList)
+			: '';
 
-    const config = [
-      { title: "ID:", value: course ? course.id : "" },
-      { title: "Duration:", value: courseDuration },
-      { title: "Created:", value: courseCreationDate },
-      { title: "Authors:", value: courseAuthors },
-    ];
+		const config = [
+			{ title: 'ID:', value: course ? course.id : '' },
+			{ title: 'Duration:', value: courseDuration },
+			{ title: 'Created:', value: courseCreationDate },
+			{ title: 'Authors:', value: courseAuthors },
+		];
 
-    return (
-      <div className={style.courseInfoWrapper}>
-        <h2 className={style.title}>{course.title}</h2>
-        <div className={style.body}>
-          <h3 className={style.subtitle}>Description:</h3>
-          <div className={style.content}>
-            <div className="w-[50%]">
-              <p>{course.description}</p>
-            </div>
-            <div className={style.line}></div>
-            <div className={style.info}>
-              {config.map((infoItem, index) => (
-                <p className={style.infoText} key={index}>
-                  <span className="font-bold">{infoItem.title}</span>
-                  <span>{infoItem.value}</span>
-                </p>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="text-right">
-          <Button text="back" onClick={handleButtonClick} />
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className={style.courseInfoWrapper}>
+				<h2 className={style.title}>{course.title}</h2>
+				<div className={style.body}>
+					<h3 className={style.subtitle}>Description:</h3>
+					<div className={style.content}>
+						<div className='w-[50%]'>
+							<p>{course.description}</p>
+						</div>
+						<div className={style.line}></div>
+						<div className={style.info}>
+							{config.map((infoItem, index) => (
+								<p className={style.infoText} key={index}>
+									<span className='font-bold'>{infoItem.title}</span>
+									<span>{infoItem.value}</span>
+								</p>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className='text-right'>
+					<Button text='back' onClick={handleButtonClick} />
+				</div>
+			</div>
+		);
+	}
 };
