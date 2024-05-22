5822aa26be940d7958e846836b716bbaf0d8c6ac
asselzhl <asselzhly@gmail.com>
2024-05-22 15:49:02 +0500

    refactor code

--- src/components/Courses/components/CourseList/CourseList.tsx
+++ src/components/Courses/components/CourseList/CourseList.tsx
@@ -1,60 +1,60 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch } from "src/store";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from 'src/store';
 
-import { convertDateToDotFormat } from "../../../../helpers/convertDateToDotFormat";
-import { getCourseDuration } from "../../../../helpers/getCourseDuration";
-import { getAuthorsName } from "../../../../helpers/getAuthorsName";
+import { convertDateToDotFormat } from '../../../../helpers/convertDateToDotFormat';
+import { getCourseDuration } from '../../../../helpers/getCourseDuration';
+import { getAuthorsName } from '../../../../helpers/getAuthorsName';
 
-import { fetchAuthors } from "../../../../store/thunks";
+import { fetchAuthors } from '../../../../store/thunks';
 
-import { CourseCard } from "../CourseCard/CourseCard";
+import { CourseCard } from '../CourseCard/CourseCard';
 import {
-  getAuthorsList,
-  getAuthorsStateStatus,
-  getCoursesStateStatus,
-  getFilteredCourses,
-} from "../../../../store/selectors";
-import { stateStatus } from "../../../../store/slices/constants";
+	getAuthorsList,
+	getAuthorsStateStatus,
+	getCoursesStateStatus,
+	getFilteredCourses,
+} from '../../../../store/selectors';
+import { stateStatus } from '../../../../store/slices/constants';
 
 export const CourseList = () => {
-  const authorsList = useSelector(getAuthorsList);
-  const filteredCourses = useSelector(getFilteredCourses);
+	const authorsList = useSelector(getAuthorsList);
+	const filteredCourses = useSelector(getFilteredCourses);
 
-  const authorsStatus = useSelector(getAuthorsStateStatus);
-  const coursesStatus = useSelector(getCoursesStateStatus);
-  const dispatch = useDispatch<AppDispatch>();
+	const authorsStatus = useSelector(getAuthorsStateStatus);
+	const coursesStatus = useSelector(getCoursesStateStatus);
+	const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(() => {
-    if (authorsStatus === stateStatus.idle) {
-      dispatch(fetchAuthors());
-    }
-  }, [authorsStatus, dispatch]);
+	useEffect(() => {
+		if (authorsStatus === stateStatus.idle) {
+			dispatch(fetchAuthors());
+		}
+	}, [authorsStatus, dispatch]);
 
-  if (
-    authorsStatus === stateStatus.succeeded &&
-    coursesStatus === stateStatus.succeeded
-  ) {
-    return (
-      <ul>
-        {filteredCourses.map((course) => {
-          const authors = getAuthorsName(course.authors, authorsList);
-          const creationDate = convertDateToDotFormat(course.creationDate);
-          const duration = getCourseDuration(course.duration);
+	if (
+		authorsStatus === stateStatus.succeeded &&
+		coursesStatus === stateStatus.succeeded
+	) {
+		return (
+			<ul>
+				{filteredCourses.map((course) => {
+					const authors = getAuthorsName(course.authors, authorsList);
+					const creationDate = convertDateToDotFormat(course.creationDate);
+					const duration = getCourseDuration(course.duration);
 
-          return (
-            <CourseCard
-              key={course.id}
-              id={course.id}
-              courseName={course.title}
-              duration={duration}
-              creationDate={creationDate}
-              description={course.description}
-              authors={authors}
-            />
-          );
-        })}
-      </ul>
-    );
-  }
+					return (
+						<CourseCard
+							key={course.id}
+							id={course.id}
+							courseName={course.title}
+							duration={duration}
+							creationDate={creationDate}
+							description={course.description}
+							authors={authors}
+						/>
+					);
+				})}
+			</ul>
+		);
+	}
 };
--- src/store/slices/courseForm/courseFormSlice.ts
+++ src/store/slices/courseForm/courseFormSlice.ts
@@ -23,10 +23,7 @@ export const courseFormSlice = createSlice({
 			const isDurationDefined = payload.duration !== undefined;
 			const isDurationNotNumber = typeof payload.duration !== 'number';
 
-			if (
-				isDurationDefined &&
-				isDurationNotNumber
-			) {
+			if (isDurationDefined && isDurationNotNumber) {
 				payload.duration = Number(payload.duration);
 			}
 			return { ...state, ...payload };
