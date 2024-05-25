a1c0af8b24009854348061bc564a3c736f65eacd
asselzhl <asselzhly@gmail.com>
2024-05-25 12:53:35 +0500

    refactor Form fields component

--- src/common/FormFieldWithError/FormFieldWithError.tsx
+++ src/common/FormFieldWithError/FormFieldWithError.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
-import { formFieldsMap } from "./formFieldsMap";
+import React from 'react';
+import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
+import { formFieldsMap } from './formFieldsMap';
 
 const style = {
-  pageInput: `py-3 px-4 leading-6 rounded border border-[#CFCFCF] focus:outline-[#007298] w-full`,
+	pageInput: `py-3 px-4 leading-6 rounded border border-[#CFCFCF] focus:outline-[#007298] w-full`,
 };
 interface ErrorMessages {
-  title: string;
-  description: string;
-  duration: string;
-  authors: string;
+	title: string;
+	description: string;
+	duration: string;
+	authors: string;
 }
 interface InputProps {
-  name: string;
-  value: string | number;
-  errorMessages: ErrorMessages;
-  onChange: (e?) => void;
+	name: string;
+	value: string | number;
+	errorMessages: ErrorMessages;
+	onChange: (e?) => void;
 }
 
 export const FormFieldWithError = ({
-  name,
-  value,
-  errorMessages,
-  onChange,
+	name,
+	value,
+	errorMessages,
+	onChange,
 }: InputProps) => {
-  const config = formFieldsMap[name];
+	const config = formFieldsMap[name];
 
-  return (
-    <div>
-      <label htmlFor={config.inputID} className="font-bold capitalize">
-        {config.labelText}
-        <input
-          type={config.type}
-          id={config.inputID}
-          placeholder={config.placeholderText}
-          name={config.name}
-          value={value}
-          className={style.pageInput}
-          onChange={onChange}
-        />
-      </label>
-      <ErrorMessage errorMessage={errorMessages[name]} />
-    </div>
-  );
+	return (
+		<div>
+			<label htmlFor={config.inputID} className='font-bold capitalize'>
+				{config.labelText}
+				<input
+					type={config.type}
+					id={config.inputID}
+					placeholder={config.placeholderText}
+					name={config.name}
+					value={value}
+					className={style.pageInput}
+					onChange={onChange}
+				/>
+			</label>
+			<ErrorMessage errorMessage={errorMessages[name]} />
+		</div>
+	);
 };
--- src/common/TextareaWithError/TextareaWithError.tsx
+++ src/common/TextareaWithError/TextareaWithError.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
-import { formFieldsMap } from "../FormFieldWithError/formFieldsMap";
+import React from 'react';
+import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
+import { formFieldsMap } from '../FormFieldWithError/formFieldsMap';
 
 const style = {
-  textarea: `py-3 px-4 leading-6 rounded border border-[#CFCFCF] focus:outline-[#007298] w-full`,
+	textarea: `py-3 px-4 leading-6 rounded border border-[#CFCFCF] focus:outline-[#007298] w-full`,
 };
 
 interface ErrorMessages {
-  title: string;
-  description: string;
-  duration: string;
-  authors: string;
+	title: string;
+	description: string;
+	duration: string;
+	authors: string;
 }
 interface TextareaProps {
-  name: string;
-  value: string;
-  errorMessages: ErrorMessages;
-  onChange: (e?) => void;
+	name: string;
+	value: string;
+	errorMessages: ErrorMessages;
+	onChange: (e?) => void;
 }
 
 export const TextareaWithError = ({
-  name,
-  value,
-  errorMessages,
-  onChange,
+	name,
+	value,
+	errorMessages,
+	onChange,
 }: TextareaProps) => {
-  const config = formFieldsMap[name];
+	const config = formFieldsMap[name];
 
-  return (
-    <div>
-      <label htmlFor={config.inputID} className="font-bold capitalize">
-        {config.labelText}
-        <textarea
-          id={config.inputID}
-          placeholder={config.placeholderText}
-          name={config.name}
-          value={value}
-          className={style.textarea}
-          onChange={onChange}
-        ></textarea>
-      </label>
-      <ErrorMessage errorMessage={errorMessages.description} />
-    </div>
-  );
+	return (
+		<div>
+			<label htmlFor={config.inputID} className='font-bold capitalize'>
+				{config.labelText}
+				<textarea
+					id={config.inputID}
+					placeholder={config.placeholderText}
+					name={config.name}
+					value={value}
+					className={style.textarea}
+					onChange={onChange}
+				></textarea>
+			</label>
+			<ErrorMessage errorMessage={errorMessages.description} />
+		</div>
+	);
 };
